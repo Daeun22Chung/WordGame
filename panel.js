d0fb206ebb015eac63f441f5a862ea6f8ca29e68
Daeun22Chung <164187336+Daeun22Chung@users.noreply.github.com>
2024-04-30 02:41:34 +0900

    Add files via upload

--- panel.js
+++ panel.js
@@ -4,8 +4,6 @@ class Panel {
     }
     draw() {
         context.fillStyle = 'rgba(255,0,0,0.8)';
-        context.resetTransform() // 변환 자체를 초기화
-        // context.setTransform(1,0,0,1,0,0);
         context.fillRect(oX-150, oY-150, 300, 300);
         context.fillStyle = "#fff";
         if(selectedBox) {
